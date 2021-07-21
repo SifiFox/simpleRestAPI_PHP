10fd9f574203cc7d02f7b324269b8f9d94260386
Dmitry Antonovich <36167942+SifiFox@users.noreply.github.com>
2021-07-21 12:36:19 +0300

    Add files via upload

---
+++ api.rest/index.php
@@ -0,0 +1,41 @@
+<?php
+header ('Content-Type: application/json');
+require 'connect.php';
+require 'functions.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$q = $_GET['q'];
+$params = explode ('/', $q);
+
+$type = $params[0];
+$id = $params[1];
+
+if($method === 'GET'){
+    if($type === 'posts'){
+        if(isset($id)){
+            getPost($connect, $id);
+        }else{
+            getPosts ($connect);
+        }
+    }
+}elseif($method === 'POST'){
+    if($type === 'posts'){
+        addPost($connect, $_POST);
+    }
+}elseif($method === 'PATCH'){
+    if($type === 'posts'){
+        if(isset($id)){
+            $data = file_get_contents ('php://input');
+            $data = json_decode ($data, true);
+            updatePost($connect, $id, $data);
+        }
+    }
+}elseif ($method === 'DELETE'){
+    if($type === 'posts'){
+        if(isset($id)){
+            deletePost($connect, $id);
+        }
+    }
+}
+
